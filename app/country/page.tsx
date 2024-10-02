e488d2b039f1982df73b77750104cd3c746189d0
Fatima salman <148888412+Jeonfatima@users.noreply.github.com>
2024-10-02 21:34:22 +0500

    Add files via upload

---
+++ app/country/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Country = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline p-4">
+        This is Countries Page:
+      </h1>
+      <ul>
+        <li className="text-2xl font-bold text-pink-600 ml-5">
+          <Link href={"/country/pakistan"}>Pakistan</Link>
+        </li>
+        <li className="text-2xl font-bold text-pink-600 ml-5">
+          <Link href={"/country/india"}>India</Link>
+        </li>
+        <li className="text-2xl font-bold text-pink-600 ml-5">
+          <Link href={"/country/china"}>China</Link>
+        </li>
+        <li className="text-2xl font-bold text-pink-600 ml-5">
+          <Link href={"/country/japan"}>Japan</Link>
+        </li>
+        <li className="text-2xl font-bold text-pink-600 ml-5">
+          <Link href={"/country/korea"}>Korea</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Country;
