2dc8559a9717a52779f7817931107fe4694be663
Fatima salman <148888412+Jeonfatima@users.noreply.github.com>
2024-10-02 21:38:33 +0500

    Add files via upload

---
+++ constant/data.tsx
@@ -0,0 +1,27 @@
+export const country_data = {
+    korea: {
+      name: "South Korea",
+      population: "51.63 million ",
+      capital: "Souel"
+    },
+    india: {
+      name: "India",
+      population: "1,38,00,00,000",
+      capital: "Dehli"
+    },
+    japan: {
+      name: "Japan",
+      population: "125,000,000",
+      capital: "Tokyo"
+    },
+    pakistan: {
+      name: "Pakistan",
+      population: "1,38,00,00,000",
+      capital: "Islamabad"
+    },
+    china: {
+      name: "China",
+      population: "1,400,000,000",
+      capital: "Beijing"
+    }
+  };
